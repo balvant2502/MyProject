02e023251725d610b5ea2545786f8b102c596ab5
NandanVadi <nandanvadi@gmail.com>
2026-01-30 12:34:21 +0530

    authentication demo

--- djangoturtorial/polls/views.py
+++ djangoturtorial/polls/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.template import loader
 from django.http import HttpResponse
-
-
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate
+from django.contrib import messages
 from .models import Question
 
 
@@ -11,6 +12,22 @@ def index(request):
     context = {"latest_question_list": latest_question_list}
     return render(request, 'polls/index.html', context)
 
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                messages.info(request, f"You are now logged in as {username}.")
+                
+            else:
+                messages.error(request,"Invalid username or password.")
+        else:
+            messages.error(request,"Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name="polls/login.html", context={"login_form":form})
 
 
 def detail(request, question_id):
